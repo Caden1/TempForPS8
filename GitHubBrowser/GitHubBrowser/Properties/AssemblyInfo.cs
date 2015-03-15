d146c621b8cf6cb2d93234003bd332f760cf1ae4
Caden1 <xbartonc@gmail.com>
2015-03-14 18:40:09 -0600

    Created GUI

--- GitHubBrowser/GitHubBrowser/Properties/AssemblyInfo.cs
+++ GitHubBrowser/GitHubBrowser/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5baee9a5-c892-4763-81fd-08596e1ed972")]
+[assembly: Guid("c1548548-2b1f-45ce-9323-cb0b1a71592e")]
 
 // Version information for an assembly consists of the following four values:
 //
